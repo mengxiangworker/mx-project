1a9f37cccbf059fbc0487131f9a7170f47b26f84
peidong.meng <peidong.meng@jiangrongxin.com>
2019-05-19 14:59:09 +0800

    更新包

--- login/src/main/java/mx/template/login/dao/UserDao.java
+++ login/src/main/java/mx/template/login/dao/UserDao.java
@@ -1,7 +1,6 @@
 package mx.template.login.dao;
 
 import mx.template.login.entity.User;
-import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
@@ -9,7 +8,6 @@ import java.util.List;
  * @author peidong.meng
  * @date 2019/5/18
  */
-@Mapper
 public interface UserDao {
 
     List<User> selectAll();
